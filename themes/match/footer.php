56a0a9692de9a05dc7e70772c724e2bd1f34c637
engza <engstrom.zach@gmail.com>
2018-07-23 22:43:16 -0500

    footer.php - after wedding the footer will read "©2018 Zach & Kelley Engstrom"

--- themes/match/footer.php
+++ themes/match/footer.php
@@ -19,7 +19,14 @@
 				<div class="row">
 					<div class="col-12">
 						<div class="credits">
-							<?php echo '&copy;' . date('Y') . ' Zach Engstrom & Kelley Vanderbeck'; ?>
+							<?php 
+								date_default_timezone_set('America/Chicago');
+								if (new DateTime() < new DateTime("2018-09-15 16:30:00")) {
+									echo '&copy;' . date('Y') . ' Zach Engstrom & Kelley Vanderbeck';
+								} else {
+									echo '&copy;' . date('Y') . ' Zach & Kelley Engstrom';
+								} 
+							?>
 						</div>
 					</div>
 				</div>
