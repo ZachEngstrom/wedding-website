5fef641d9cf05554f7bfec3d2c1b5218c4fac884
engza <engstrom.zach@gmail.com>
2018-07-23 21:16:03 -0500

    RSVP - referenced files needed for plugin

--- plugins/match-rsvp/match-rsvp.php
+++ plugins/match-rsvp/match-rsvp.php
@@ -7,6 +7,8 @@
  * Author URI: http://zachengstrom.com
  **/
 
+include_once plugin_dir_path( __FILE__ ) . "inc/db-setup.php";
+include_once plugin_dir_path( __FILE__ ) . "inc/admin-setup.php";
 include_once plugin_dir_path( __FILE__ ) . "inc/ui-deliver.php";
 include_once plugin_dir_path( __FILE__ ) . "inc/ui-form.php";
 
